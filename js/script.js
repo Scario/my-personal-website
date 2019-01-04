b34aa97b5044dbec808102c53b51d07697c07377
Davide <scario1988@gmail.com>
2019-01-04 19:38:56 +0100

    Dinamic Modal Content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var square = document.querySelectorAll(".square a");
+var square = document.querySelectorAll(".square-back");
 
 var closeBtn = document.getElementById("close");
 
@@ -13,5 +13,9 @@ for (i=0; i<square.length; i++) {
     square[i].addEventListener("click", function(e){
         e.preventDefault;
         modal.classList.add("open");
+        var modalContainer = modal.getElementsByClassName("modal-inner")[0];
+        var titleContent = e.target.previousElementSibling.innerHTML
+        var content = e.target.nextElementSibling.innerHTML;
+        modalContainer.innerHTML = "<h2>"+titleContent+"</h2>" + content;
     })
 }
